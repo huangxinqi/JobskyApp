40b9951ce5c760e166026c365ec3dc319cb3da9d
wxz1234 <1491039324@qq.com>
2016-01-15 17:29:26 +0800

    Revert "提交了web代码"
    This reverts commit 9f562ae7f7841279d8a231ccb92d5d388cfb473a.

--- program/web/JobskyApp/src/edu/csu/action/UserAction.java
+++ program/web/JobskyApp/src/edu/csu/action/UserAction.java
@@ -1,12 +1,7 @@
 package edu.csu.action;
 
-import com.opensymphony.xwork2.ActionSupport;
-
 /**
  * Created by huangxinqi on 2016/1/14.
  */
-public class UserAction  extends ActionSupport{
-    public String login(){
-        return INPUT;
-    }
+public class UserAction {
 }
